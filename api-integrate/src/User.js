0a802ea18884d35faf4842e0e1481312b638493a
kakao_ent <luis.me>
2020-04-17 19:16:09 +0900

    react-async tkdyd

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
 
-async function getUser(id) {
+async function getUser({id}) {
     const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
 
     return response.data;
@@ -11,10 +11,17 @@ async function getUser(id) {
 function User({ id }) {
 
     // [id]를 해줌으로써 id 값이 바뀔때마다 호출 해주겠다는 의미
-    const [state] = useAsync(() => getUser(id), [id]);
-    const { loading, data: user, error } = state;
+    const {
+        data: user,
+        error,
+        isLoading
+    } = useAsync({
+        promiseFn: getUser,
+        id,
+        watch: id,
+    });
 
-    if (loading) return <div>로딩중...</div>;
+    if (isLoading) return <div>로딩중...</div>;
     if (error) return <div>에러가 발생했습니다...</div>;
     if (!user) return null;
 
